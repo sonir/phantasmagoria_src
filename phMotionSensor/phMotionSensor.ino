492bf30a1f7b4cd57a451c2bb9f9e7f1e8b26ae6
sonir <isana@sonir.jp>
2015-02-17 19:40:15 +0900

    ver0.2 add lowpass filter

--- phMotionSensor/phMotionSensor.ino
+++ phMotionSensor/phMotionSensor.ino
@@ -1,13 +1,14 @@
 
 //Set number of connected sensor
-const int sensor_num = 5;
+const int sensor_num = 1;
 //Reflash rate (in MS)
 const int reflesh_rate = 50;
-//lowpass_freq
-const int lowpass_freq = 1;
+//lowpass_length
+const int lowpass_length = 10;
+const float threshold = 0.97;
 
 //buff for lowpass to avoid chuttering
-float lowpass_buf[sensor_num][lowpass_freq];
+float lowpass_buf[sensor_num][lowpass_length];
 //Init the pin number of digital in
 int sensors [] = {2,5,8,12,13}; //def 2,3,4,5,6
 //Array for storage the get value
@@ -15,12 +16,65 @@ int sensors_val[sensor_num];
 int tmp_sensors_val[sensor_num];
 
 
+
+int lowpass(int sensor_id, float fVal){
+
+	float subtotal=0.;
+ 	float avg = 0.;
+	char s[16];
+	char t[16];
+
+
+	//Stack
+	for(int i=(lowpass_length-1);i>=0;i--){
+
+		if(i==0){ //If the index is 0, store the newest value
+
+			lowpass_buf[sensor_id][i]=(int)fVal%(int)10.0f; //decode the value
+			continue;
+
+		}
+
+		//Slide All values
+		lowpass_buf[sensor_id][i] = lowpass_buf[sensor_id][i-1];
+	}
+
+
+	//Sum total
+	for(int i=0;i<lowpass_length;i++){
+	    subtotal+=lowpass_buf[sensor_id][i];
+
+	 //  	int val = (int)100*lowpass_buf[sensor_id][i];
+		// sprintf(t, "sensor[%d][%d] is  %d" , sensor_id, i, val);
+		// Serial.println(t);
+
+
+	}
+
+
+	//Calc AVG
+	avg = subtotal/(float)lowpass_length;
+	// int avg_i = (int)100*avg;
+	// sprintf(t, "Sensor[%d] : avg is  %d" , sensor_id, avg_i);
+	// Serial.println(t);
+	
+
+	if(avg>=threshold || (1.0-avg)>=threshold){
+	    // Serial.println("true");
+	    return 1;
+	}else{
+	    // Serial.println("false");
+	    return 0;
+	}
+
+}
+
 void getParams()
 {
   for(int i=0; i<sensor_num; i++)
   {
-     //Storage the get value
-     int got_val= digitalRead(sensors[i]);     
+    //Storage the get value
+    int got_val= digitalRead(sensors[i]);     
     //Invert the val
     got_val = 1-got_val;
     //Encode the val for serial ( index+ x 10 = place ID , x 1 = sensor value)
@@ -33,65 +87,33 @@ void sendParams()
   
   for(int i=0;i<sensor_num; i++)
   {
-      if( tmp_sensors_val[i]^sensors_val[i] ){ //Check Difference
+      //if( tmp_sensors_val[i]^sensors_val[i] ){ //Check Difference
 
         //If the value was changed
-        sensors_val[i]=tmp_sensors_val[i]; //Store the new value
+        //sensors_val[i]=tmp_sensors_val[i]; //Store the lowpass_length value
         //Check is it chuttering or not with Lowpass
-      	float chuttering_chk = lowpass(i,tmp_sensors_val[i]);
+      	if( !lowpass(i,(float)tmp_sensors_val[i]) ){
 
-      	if( chuttering_chk!=1.0 || chuttering_chk!=0.0){
-      		Serial.println("detect chuttering ignore.");
-      		continue;
+      		// Serial.println("detect chuttering ignore.");
+
+      	} else{
+
+      		if( tmp_sensors_val[i]^sensors_val[i] ){
+      	        sensors_val[i]=tmp_sensors_val[i]; //Store the lowpass_length value
+		        Serial.println(sensors_val[i]); //Send the lowpass_length value to serial port 
+		        delay(20);       
+		
+      		}
+
       	}
 
-        Serial.println(sensors_val[i]); //Send the new value to serial port 
-        delay(20);       
-      }
+      //}
       
   }    
     
-   
 
 }
 
-
-float lowpass (int sensor_id , int iVal){
-
-	// float *pBuf = lowpass_buf[sensor_id];
-	float sub_total = 0.;
-
-	// //Reflesh buff
-	// for(int i=lowpass_freq; i>0; i--){
-
-	// 	pBuf[i]=pBuf[i-1];
-	// 	sub_total += pBuf[i]; //Add all value for calc avg
-
-	// }
-
-	// pBuf[0] = (float)iVal;
-	// sub_total += pBuf[0];
-
-
-	//Reflesh buff
-	for(int i=lowpass_freq; i>0; i--){
-
-		lowpass_buf[sensor_id][i] = lowpass_buf[sensor_id][i-1];
-		sub_total += lowpass_buf[sensor_id][i]; //Add all value for calc avg
-
-	}
-
-	lowpass_buf[sensor_id][0] = (float)iVal;
-	sub_total += lowpass_buf[sensor_id][0];
-
-
-	//Calc AVG
-	return (sub_total/(float)sensor_id);
-
-
-
-
-}
 
 
 void setup()
@@ -117,7 +139,7 @@ void setup()
    //Init Lowpass buffer
    for(int i=0; i<sensor_num;i++){
 
-   		for(int j=0; j<lowpass_freq; j++){
+   		for(int j=0; j<lowpass_length; j++){
 
    			lowpass_buf[i][j]=0.;
 
